195c36a461850ea20f5c11ca1c2f5d47a1f6307e
marzieh <marziehsepehr@gmail.com>
2020-01-21 09:22:37 +0330

    add token for user

--- userservice/receivers.py
+++ userservice/receivers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save,post_delete
 from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 from .models import Profile
 
@@ -10,8 +11,11 @@ def create_user_profile(sender, instance, created, **kwargs):
     if not instance.is_staff:
         if created :
             Profile.objects.create(user=instance)
+            Token.objects.create(user=instance)
         instance.profile.save()
 
 # @receiver(post_delete, sender=User)
 # def delete_user_profile(sender, instance=None, **kwargs):
 #     instance.profile.delete(user=instance)
+
+
